66e0f73d3286c7a461701239888408f81e2b5041
Adam Szemere <sephxdnb@gmail.com>
2020-10-26 13:37:29 +0100

    connection template

---
+++ src/main/java/com/codecool/stockhub/service/HTTPConnection.java
@@ -0,0 +1,40 @@
+package com.codecool.stockhub.service;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Component
+public class HTTPConnection {
+
+
+    public HTTPConnection() throws IOException {
+        URL url = new URL("https://finnhub.io/api/v1/stock/symbol?exchange=US&token=bu21mlf48v6u9tetnbt0");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        System.out.println();
+        int status = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(content);
+
+        con.disconnect();
+
+    }
+
+
+}
