bd2bfeabad76f6c74905bbcdd7d666236e98075a
Bence <borsi92@gmail.com>
2020-10-27 17:37:10 +0100

    refoctered HTTPConnection to use Spring boot solutions

---
+++ src/main/java/com/codecool/stockhub/service/CompanyList.java
@@ -0,0 +1,2 @@
+package com.codecool.stockhub.service;public class CompanyList {
+}
--- src/main/java/com/codecool/stockhub/service/HTTPConnection.java
+++ src/main/java/com/codecool/stockhub/service/HTTPConnection.java
@@ -1,43 +1,16 @@
 package com.codecool.stockhub.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @Component
 public class HTTPConnection {
 
-    private final StringBuilder content;
+    public String getContent(String api) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(api, String.class);
 
-    public HTTPConnection() throws IOException {
-        URL url = new URL("https://finnhub.io/api/v1/stock/symbol?exchange=US&token=bu21mlf48v6u9tetnbt0");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        System.out.println();
-        int status = con.getResponseCode();
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-
-        content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
-
-        System.out.println(content);
-
-        con.disconnect();
-
-    }
-
-    public StringBuilder getContent() {
-        return content;
     }
 }
