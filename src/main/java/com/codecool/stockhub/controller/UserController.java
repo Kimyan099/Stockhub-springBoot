bcfd3eba6b9b39375b46df9bae7d5f39c2b64c9f
Getulus <tamasgaj@gmail.com>
2020-10-27 20:27:51 +0100

    add user endpoint

--- src/main/java/com/codecool/stockhub/controller/UserController.java
+++ src/main/java/com/codecool/stockhub/controller/UserController.java
@@ -4,9 +4,11 @@ package com.codecool.stockhub.controller;
 import com.codecool.stockhub.model.User;
 import com.codecool.stockhub.service.UserList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,10 +17,22 @@ public class UserController {
     @Autowired
     private UserList userList;
 
+
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/add")
-    public User addUser(@RequestBody User user){
+    public User addUser(@RequestBody User user, HttpServletResponse response) {
         userList.registerUser(user);
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+        response.setStatus(200);
         return user;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setStatus(200);
+        return userList.getUsers();
     }
 
 }
