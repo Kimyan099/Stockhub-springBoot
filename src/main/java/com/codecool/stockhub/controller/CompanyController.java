1dcee6957a920df5a57b86e44ded2c5433b67c0c
Bence <borsi92@gmail.com>
2020-10-27 17:41:24 +0100

    company controller returns filtered Json Array

--- src/main/java/com/codecool/stockhub/controller/CompanyController.java
+++ src/main/java/com/codecool/stockhub/controller/CompanyController.java
@@ -1,13 +1,17 @@
 package com.codecool.stockhub.controller;
 
 
+import com.codecool.stockhub.model.Company;
+import com.codecool.stockhub.service.CompanyList;
 import com.codecool.stockhub.service.HTTPConnection;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class CompanyController {
@@ -15,14 +19,16 @@ public class CompanyController {
     @Autowired
     private HTTPConnection httpConnection;
 
-
-
+    @Autowired
+    private CompanyList companyList;
 
     @GetMapping("/companies")
-    public StringBuilder companyList(HttpServletResponse response) throws IOException {
+    public List<Company> companyList(HttpServletResponse response) throws IOException, JSONException {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setStatus(200);
-        return httpConnection.getContent();
+        String jsonResponse = httpConnection.getContent("https://finnhub.io/api/v1/stock/symbol?exchange=US&token=bu21mlf48v6u9tetnbt0");
+        companyList.filterData(jsonResponse);
+        return companyList.getCompanies();
     }
 
 
