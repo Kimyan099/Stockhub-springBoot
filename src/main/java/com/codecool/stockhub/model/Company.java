b65617c01dcae33de8c09dd796ea1bee9b3ceeb9
Bence <borsi92@gmail.com>
2020-10-27 17:38:54 +0100

    created company model class

--- src/main/java/com/codecool/stockhub/model/Company.java
+++ src/main/java/com/codecool/stockhub/model/Company.java
@@ -1,2 +1,31 @@
-package com.codecool.stockhub.model;public class Company {
+package com.codecool.stockhub.model;
+
+public class Company {
+
+    private String description;
+    private String symbol;
+
+    public Company(String description, String symbol) {
+        this.description = description;
+        this.symbol = symbol;
+    }
+
+    public Company() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
 }
