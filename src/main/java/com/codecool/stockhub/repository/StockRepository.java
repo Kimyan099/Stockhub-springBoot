2a227b077681900b811d7639baee0f80fdbdeec7
Bence <borsi92@gmail.com>
2020-11-09 17:58:02 +0100

    added StockRepository

--- src/main/java/com/codecool/stockhub/repository/StockRepository.java
+++ src/main/java/com/codecool/stockhub/repository/StockRepository.java
@@ -1,2 +1,7 @@
-package com.codecool.stockhub.repository;public interface StockRepository {
+package com.codecool.stockhub.repository;
+
+import com.codecool.stockhub.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<Stock, Long> {
 }
