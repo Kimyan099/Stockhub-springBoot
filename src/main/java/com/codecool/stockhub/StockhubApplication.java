8964e21dd2db57f64736cf878d1d0a95ad4fe066
Bence <borsi92@gmail.com>
2020-11-10 20:08:02 +0100

    added admin user

--- src/main/java/com/codecool/stockhub/StockhubApplication.java
+++ src/main/java/com/codecool/stockhub/StockhubApplication.java
@@ -1,8 +1,9 @@
 package com.codecool.stockhub;
 
 import com.codecool.stockhub.logger.ExceptionLog;
-import com.codecool.stockhub.model.Company;
-import com.codecool.stockhub.repository.CompanyRepository;
+import com.codecool.stockhub.model.Client;
+import com.codecool.stockhub.model.Stock;
+import com.codecool.stockhub.repository.ClientRepository;
 import com.codecool.stockhub.service.CompanyList;
 import com.codecool.stockhub.service.HTTPConnection;
 import org.slf4j.Logger;
@@ -15,10 +16,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 public class StockhubApplication {
@@ -36,6 +33,9 @@ public class StockhubApplication {
     @Autowired
     private HTTPConnection httpConnection;
 
+    @Autowired
+    private ClientRepository clientRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(StockhubApplication.class, args);
@@ -46,6 +46,21 @@ public class StockhubApplication {
     public CommandLineRunner init() {
 
         return args -> {
+            Stock apple = Stock.builder()
+                    .price(100)
+                    .symbol("aapl")
+                    .name("Apple")
+                    .build();
+
+            Client admin = Client.builder()
+                    .name("admin")
+                    .email("admin@gmail.com")
+                    .password("123")
+                    .stock(apple)
+                    .build();
+
+            apple.setClient(admin);
+            clientRepository.saveAndFlush(admin);
             String jsonResponse = httpConnection.getContent(COMPANIES_URL);
             try {
                 companyList.filterData(jsonResponse);
