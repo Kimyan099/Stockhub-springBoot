8dc321b6f20720f9692be5648a90bb60c84b9894
Bence <borsi92@gmail.com>
2021-01-03 16:41:00 +0100

    added Swagger2 and Docket Bean to news-registry

--- news-registry/src/main/java/com/codecool/newsregistry/NewsRegistryApplication.java
+++ news-registry/src/main/java/com/codecool/newsregistry/NewsRegistryApplication.java
@@ -9,8 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class NewsRegistryApplication {
 
 	private static final String NEWS_URL = "https://finnhub.io/api/v1/news?category=general&token=bu2rf9f48v6pqlhnnvtg";
@@ -42,5 +48,14 @@ public class NewsRegistryApplication {
 				exceptionLog.log(e);
 			}
 		};
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 }
