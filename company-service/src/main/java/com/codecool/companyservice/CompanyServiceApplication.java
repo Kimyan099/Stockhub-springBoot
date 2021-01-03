c75758ed8191e86042791fdc8e62ea4e5609d3b2
Bence <borsi92@gmail.com>
2021-01-03 16:39:36 +0100

    added Swagger2 and Docket Bean to company-service

--- company-service/src/main/java/com/codecool/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/com/codecool/companyservice/CompanyServiceApplication.java
@@ -10,6 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -46,5 +50,14 @@ public class CompanyServiceApplication {
 				exceptionLog.log(e);
 			}
 		};
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 }
